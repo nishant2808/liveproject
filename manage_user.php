15e41c3a88a972a404c65b951a7b577e6664e419
nishant2808 <nishant.thakur2328@gmail.com>
2024-04-16 13:21:37 +0530

    changing

--- manage_user.php
+++ manage_user.php
@@ -172,5 +172,4 @@ $roles =  $result->fetch_all(MYSQLI_ASSOC);?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
